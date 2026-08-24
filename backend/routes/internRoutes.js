a79ea19766bf86c7b5a2d194195e7296a77e07e1
Omer Shireen <omer7sce@gmail.com>
2026-08-24 22:09:13 +0500

    Implement task management and admin authorization

--- backend/routes/internRoutes.js
+++ backend/routes/internRoutes.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const {getAllInterns, getInternById, updateIntern, deactivateIntern} = require("../controllers/internController");
+
 const protect = require("../middleware/authMiddleware")
+const admin = require("../middleware/adminMiddleware")
+
 const router = express.Router();
 
-router.get("/", protect, getAllInterns); 
+router.get("/", protect, admin,  getAllInterns); 
 router.get("/:id", protect, getInternById); 
 router.put("/:id", protect, updateIntern); 
 router.patch("/:id/status", protect, deactivateIntern); 
--- backend/routes/taskRoutes.js
+++ backend/routes/taskRoutes.js
@@ -4,19 +4,22 @@ const {
     createTask,
     getAllTasks,
     getMyTasks,
-    updateTaskStatus
+    updateTaskStatus,
+    deleteTask
 } = require("../controllers/taskController");
 
 const protect = require("../middleware/authMiddleware");
-
+const admin = require("../middleware/adminMiddleware")
 const router = express.Router();
 
-router.post("/", protect, createTask);
+router.post("/", protect, admin, createTask);
 
-router.get("/", protect, getAllTasks);
+router.get("/", protect, admin, getAllTasks);
 
 router.get("/my", protect, getMyTasks);
 
 router.patch("/:id/status", protect, updateTaskStatus);
+
+router.delete("/:id", protect, admin, deleteTask);
 
 module.exports = router;
