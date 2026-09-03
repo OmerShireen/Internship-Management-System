393447c721273bed8b933d6e3e0e325affad8633
Omer Shireen <omer7sce@gmail.com>
2026-09-04 00:44:11 +0500

    Created components routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,33 +3,55 @@ import Login from "./pages/Login"
 import Register from "./pages/Register"
 import AdminDashboard from "./pages/AdminDashboard"
 import InternDashboard from "./pages/InternDashboard"
+import Interns from "./pages/Interns"
+import Tasks from "./pages/Tasks"
 import "./App.css"
- 
+import Progress from "./pages/Progress"
+import Submissions from "./pages/Submissions"
+
 
 function App() {
-  
+
 
   return (
     <>
-    <BrowserRouter>
-    <Routes>
-      <Route 
-        path="/"
-        element={ <Login/>}/> 
+      <BrowserRouter>
+        <Routes>
+          <Route
+            path="/"
+            element={<Login />} />
 
-       <Route
-       path="/register"
-       element={<Register/>}/> 
+          <Route
+            path="/register"
+            element={<Register />} />
 
-       <Route
-        path="/admin-dashboard"
-        element={ <AdminDashboard/>}/>
-       <Route
-        path="/intern-dashboard"
-        element={<InternDashboard />}/> 
-    </Routes>
-    </BrowserRouter>
-          </>
+          <Route
+            path="/admin-dashboard"
+            element={<AdminDashboard />} />
+
+          <Route
+            path="/interns"
+            element={<Interns />} />
+
+          <Route
+            path="/tasks"
+            element={<Tasks />} />
+
+          <Route
+            path="/progress"
+            element={<Progress />} />
+
+          <Route
+            path="/submissions"
+            element={<Submissions />} />
+
+          <Route
+            path="/intern-dashboard"
+            element={<InternDashboard />}
+          />
+        </Routes>
+      </BrowserRouter>
+    </>
   )
 }
 
